7bc4d2bf0d908d777bd000db289fef552e7dc384
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 14:03:37 +0300

    Added some markupt to the react component

--- hooks/src/UseRef.jsx
+++ hooks/src/UseRef.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 
 const UseRef = () => {
   return (
-    <div>UseRef</div>
+    <div>
+        <h1>Pedro</h1>
+        <input type="text" name="" id="" placeholder='Ex...' />
+        <button>Change Name</button>
+    </div>
   )
 }
 
