c13679e92fba7056f4218c4829126d98ac53a66c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 12:26:46 +0300

    Created a skeleton for the reducer function

--- hooks/src/UseReducer.jsx
+++ hooks/src/UseReducer.jsx
@@ -1,6 +1,10 @@
 import React, { useReducer } from 'react';
 
 
+const reducer = (state, action) => {
+    
+};
+
 const UseReducer = () => {
     const [state, dispatch] = useReducer(reducer, {count: 0, showText: true});
 
