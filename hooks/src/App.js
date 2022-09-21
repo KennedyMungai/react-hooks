07bb19380f2883f297af86e854193787d15e5a74
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 12:38:14 +0300

    Called the useReducer component from the App.js component

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UseState from './UseState';
+import UseReducer from './UseReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
+      <UseReducer />
     </div>
   );
 }
