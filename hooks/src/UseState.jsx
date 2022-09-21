c603752da936ac6ea759ca1ce302facfdb2c192c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 12:03:57 +0300

    Removed some of the unnecessary lines of code

--- hooks/src/UseState.jsx
+++ hooks/src/UseState.jsx
@@ -2,14 +2,11 @@ import React, { useState } from 'react';
 
 
 const UseState = () => {
-    let number = 0;
     const [counter, setCounter] = useState(0);
-
-    number = counter;
 
     return (
         <div>
-            <button onClick={() => setCounter(number++)}>{counter}</button>
+            <button>{counter}</button>
         </div>
     )
 }
