fb954dd12e4a5eb80bcc31014064922d3eefc801
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 14:11:20 +0300

    Added the value attribute

--- hooks/src/UseRef.jsx
+++ hooks/src/UseRef.jsx
@@ -5,7 +5,7 @@ const UseRef = () => {
     const inputRef = useRef(null);
 
     const onClick = () => {
-        console.log(inputRef.current);
+        console.log(inputRef.current.value);
     };
 
     return (
