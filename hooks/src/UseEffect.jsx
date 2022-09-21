16cf2d46a27bd4d61de21fa734afacac74c86f96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 14:00:36 +0300

    Added the data and count states

--- hooks/src/UseEffect.jsx
+++ hooks/src/UseEffect.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 
 
 const UseEffect = () => {
+    const [data, setData] = useState("");
+    const [count, setCount] = useState(0);
+
     useEffect(() => {
         const comments = axios.get("https://jsonplaceholder.typicode.com/comments")
                         .then((response) => { console.log(response.data) });
